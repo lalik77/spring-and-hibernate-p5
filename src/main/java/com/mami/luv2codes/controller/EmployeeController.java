1405e28e2c425c63a3ff5739240e614c58d27ac6
Alexander Mami <algru81@gmail.com>
2022-01-09 15:51:14 +0300

    add button Add Employee

--- src/main/java/com/mami/luv2codes/controller/EmployeeController.java
+++ src/main/java/com/mami/luv2codes/controller/EmployeeController.java
@@ -5,15 +5,9 @@ import com.mami.luv2codes.model.Employee;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 @RequestMapping("/employees")
@@ -27,53 +21,16 @@ public class EmployeeController {
     this.employeeService = employeeService;
   }
 
-  //expose "/employees" and return lis of employees
   @GetMapping("/all")
-  public List<Employee> getAllEmployees() {
-    return employeeService.findAll();
-  }
+  public String getAllEmployees(Model model) {
 
-  @GetMapping("/employees/{id}")
-  public Employee getEmployee(@PathVariable("id") int id) {
+    List<Employee> employees = employeeService.findAll();
 
-    Employee employee = employeeService.findById(id);
+    model.addAttribute("employees",employees);
 
-    if (employee == null) {
-      throw new RuntimeException("Employee id not found - " + id);
-    }
-    return employee;
-  }
-  @PostMapping("/employees/")
-    public Employee addEmployee(@RequestBody Employee employee) {
-
-      // also just in case they pass an id in JSON .. set id to
-      employee.setId(0);
-      // this is to force a save of new item ... instead of update
-      employeeService.save(employee);
-      return employee;
-  }
-
-  @PutMapping("/employees/")
-  public Employee updateEmployee(@RequestBody Employee employee) {
-    // this is to force a save of new item ... instead of update
-
-    employeeService.save(employee);
-    return employee;
+    return "employees/all-employees";
   }
 
 
-  @DeleteMapping("/employees/{id}")
-  public String deleteEmployeeById( @PathVariable int id) {
-    // this is to force a save of new item ... instead of update
-
-    Employee temp = employeeService.findById(id);
-
-    if (temp == null) {
-      throw new RuntimeException("Employee with " + id + " does not exist in DB");
-    }
-
-    employeeService.deleteById(id);
-    return "Deleted Employee with id : " + id + " from DB";
-  }
 }
 
